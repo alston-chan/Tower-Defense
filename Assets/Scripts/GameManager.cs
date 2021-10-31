7876b5a6bd53ebb3810892290e12df01b676535b
Stephen Ip <91226924+stephen-ip@users.noreply.github.com>
2021-10-31 14:20:24 -0700

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -36,7 +36,7 @@ public class GameManager : Singleton<GameManager>
     public bool BuyTower()
     {
         // int tower_cost = selectedTower.price;  // pass in tower cost once implemented
-        int towe_cost = 100
+        int tower_cost = 100;
         if (PlayerStats.Fish >= tower_cost)  // do nothing if can't afford tower
         {
             PlayerStats.Fish -= tower_cost;  // TESTING: every time a tower is placed, 100 fish (currency) will be subtracted.
